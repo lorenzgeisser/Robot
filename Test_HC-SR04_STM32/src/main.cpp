1d85dad8c7b5bfb74609c16dbb379f100adb0bfe
lorenzgeisser <geisserlorenz@gmail.com>
2020-04-14 18:02:51 +0200

    work in progress

--- Test_HC-SR04_STM32/src/main.cpp
+++ Test_HC-SR04_STM32/src/main.cpp
@@ -3,19 +3,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define TRIG_PIN PB12
+#define ECHO_PIN_RIGHT PB13
+#define ECHO_PIN_FRONT PB14
+#define ECHO_PIN_LEFT PB15
+#define TEMPERATURE_ULTRASONIC 20
+#define MAX_DISDANCE_ULTRASONIC 300
+
+HCSR04 ultrasonicSensorRight(TRIG_PIN, ECHO_PIN_RIGHT, TEMPERATURE_ULTRASONIC, MAX_DISDANCE_ULTRASONIC);
+HCSR04 ultrasonicSensorFront(TRIG_PIN, ECHO_PIN_FRONT, TEMPERATURE_ULTRASONIC, MAX_DISDANCE_ULTRASONIC);
+HCSR04 ultrasonicSensorLeft(TRIG_PIN, ECHO_PIN_LEFT, TEMPERATURE_ULTRASONIC, MAX_DISDANCE_ULTRASONIC);
+
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
-
-HCSR04 ultrasonicSensor1(PB10, PB11, 20, 300);
-HCSR04 ultrasonicSensor2(PB13, PB12, 20, 300);
-
-float distance1 = 0;
-float distance2 = 0;
 
 void setup()
 {
-
-    ultrasonicSensor1.begin();
-    ultrasonicSensor2.begin();
+    ultrasonicSensorRight.begin();
+    ultrasonicSensorFront.begin();
+    ultrasonicSensorLeft.begin();
 
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     display.clearDisplay();
@@ -30,17 +35,22 @@ void setup()
 
 void loop()
 {
-    distance1 = ultrasonicSensor1.getDistance();
     delay(100);
-    distance2 = ultrasonicSensor2.getDistance();
+    float distaanceRight = ultrasonicSensorRight.getDistance();
+    delay(100);
+    float distaanceFront = ultrasonicSensorFront.getDistance();
+    delay(100);
+    float distaanceLeft = ultrasonicSensorLeft.getDistance();
 
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    display.print(distance1);
+    display.print(distaanceRight);
     display.println(" cm");
-    display.print(distance2);
+    display.print(distaanceFront);
+    display.println(" cm");
+    display.print(distaanceLeft);
     display.println(" cm");
     display.display();
 
